d4d24c55f9effb5c8be3bb777a415cd477532d6c
LOWERCASE <lowercase.inbox@gmail.com>
2021-07-21 22:21:55 -0400

    nav bar functional!

--- src/components/header.js
+++ src/components/header.js
@@ -2,32 +2,49 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
+// const StyledLink = styled(Link)`
+// 	color: White;
+// 	text-decoration: none;
+// 	margin: 1rem;
+// `;
+
+// .navlink {
+// 	color: White;
+// 	text-decoration: none;
+// 	margin: `1rem`
+// }
+
 const Header = ({ siteTitle }) => (
 	<header
-	style={{
-		background: `rebeccapurple`,
-		marginBottom: `1.45rem`,
-	}}
+		style={{ background: `rebeccapurple`, marginBottom: `1.45rem` }}
 	>
-	<div
-	style={{
-		margin: `0 auto`,
-		maxWidth: 960,
-		padding: `1.45rem 1.0875rem`,
-	}}
-	>
-	<h1 style={{ margin: 0 }}>
-	<Link
-	to="/"
-	style={{
-		color: `white`,
-		textDecoration: `none`,
-	}}
-	>
-	{siteTitle}
-	</Link>
-	</h1>
-	</div>
+		<div
+			class="topnav"
+			style={{ padding: `1.45rem 1.0875rem` }} // margin: `0 auto`, maxWidth: 960,
+		>
+			<h1 style={{ margin: 0 }}>
+				<Link
+					to="/"
+					style={{ margin: `1rem`, color: `white`, textDecoration: `none`}}
+				>
+					{siteTitle}
+				</Link>
+				
+				<Link
+					to="/page-2/"
+					style={{ margin: `1rem`, color: `white`, textDecoration: `none`}}
+				>
+					Page 2
+				</Link>
+				
+				<Link
+					to="/using-typescript/"
+					style={{ margin: `1rem`, color: `white`, textDecoration: `none`}}
+				>
+					Using Typescript
+				</Link>
+			</h1>
+		</div>
 	</header>
 )
 
@@ -36,7 +53,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-	siteTitle: ``,
+	siteTitle: `siteTitle`,
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,35 +8,34 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-		
-		return (
-			<>
-			
+	
+	return (
+		<>
 			<Header siteTitle={`Home`} />
 			<div
-			style={{
-				margin: `0 auto`,
-				maxWidth: 960,
-				padding: `0 1.0875rem 1.45rem`,
-			}}
-			>
-			<main>{children}</main>
-			<footer
-			style={{
-				marginTop: `2rem`,
-			}}
-			>
-			© {new Date().getFullYear()}, Built with
-			{` `}
-			<a href="https://www.gatsbyjs.com">Gatsby</a>
-			</footer>
+					style={{
+						margin: `0 auto`,
+						maxWidth: 960,
+						padding: `0 1.0875rem 1.45rem`,
+					}}
+				>
+				<main>{children}</main>
+				<footer
+					style={{
+						marginTop: `2rem`,
+					}}
+				>
+					© {new Date().getFullYear()}, Built with
+					{` `}
+					<a href="https://www.gatsbyjs.com">Gatsby</a>
+				</footer>
 			</div>
-			</>
-		)
-	}
-	
-	Layout.propTypes = {
-		children: PropTypes.node.isRequired,
-	}
-	
-	export default Layout
+		</>
+	)
+}
+
+Layout.propTypes = {
+	children: PropTypes.node.isRequired,
+}
+
+export default Layout
